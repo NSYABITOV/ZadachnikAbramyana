361e1d9eac77870c8aa97f5b0c20220a7e4b3a58
Niyaz Syabitov <nsyabitov@mail.ru>
2024-02-26 12:18:11 +0300

    Среднее арифметическое двух чисел

---
+++ begin8.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+/*
+Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2
+*/
+int main(){
+  int a, b;
+  cout << "Введите два числа: ";
+  cin >> a >> b;
+  cout << "Среднее арифметическое: " << (a+b)/2 << endl;
+}
