46926134eb3f9ddae38071a10ddf5e00ec778f8c
Niyaz Syabitov <nsyabitov@mail.ru>
2024-02-27 14:56:44 +0300

    Длина отрезков и их сумма

---
+++ begin17.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream> 
+#include <valarray>
+using namespace std;
+
+int main(){
+  int A,B,C,AC,BC ;
+  cout <<" Введите размер 3 точек: ";
+  AC = abs(A-B);
+  BC = abs(B-C);
+  cout << "Длина АС: " << AC << endl;
+  cout << "Длина BC: " << BC << endl;
+  cout << "Сумма длина АС и ВС: " << AC + BC << endl;
+
+}
